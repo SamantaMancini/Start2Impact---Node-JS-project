7cc8965ff58c45024143b03415ba1558fff32e2f
Sassa1993 <118011618+MiraiTsukiGames@users.noreply.github.com>
2024-01-31 20:14:26 +0100

    up

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,13 @@
 //Global Error Handling Middleware
 const AppError = require('../utils/appError');
 
+// Handle casting error from MongoDB
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
+
+// Handle duplicate fields error from MongoDB
 const handleDuplicateFieldsDB = (err) => {
   const key = { ...Object.keys(err.keyValue) };
   const value = { ...Object.values(err.keyValue) };
@@ -12,12 +15,14 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+// Handle validation error from MongoDB
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
 
+// Send detailed error message in development environment
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -27,14 +32,13 @@ const sendErrorDev = (err, res) => {
   });
 };
 
+// Send simplified error message in production environment
 const sendErrorProduction = (err, res) => {
-  // Operational Error ,trusted error : send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
-    // Programming for the unknown error : don't leak error details to client
   } else {
     res.status(500).json({
       status: 'error',
@@ -43,8 +47,9 @@ const sendErrorProduction = (err, res) => {
   }
 };
 
+// Global error handling middleware
 module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500; // internal server error 500
+  err.statusCode = err.statusCode || 500; 
   err.status = err.status || 'error';
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
@@ -52,10 +57,10 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     error.name = err.name; //
     error.message = err.message; //
-    if (error.name === 'CastError') error = handleCastErrorDB(error); // "CastError" mongoose when _id for find is no valide
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error); // duplicate value in a UNIQUE field
+    if (error.name === 'CastError') error = handleCastErrorDB(error); 
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error); 
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
-    sendErrorProduction(error, res); // send to client
+    sendErrorProduction(error, res); 
   }
 };
